a9b8aa6976ae2e8807a3d138fe8da1855d3a9fa3
Aleksandr Berezin <32334657+denura@users.noreply.github.com>
2020-01-28 22:09:02 +0300

    1.08.Find3HighestValuesInDictionary.py:6:58: C0303: Trailing whitespace (trailin g-whitespace)

--- 1.08.Find3HighestValuesInDictionary.py
+++ 1.08.Find3HighestValuesInDictionary.py
@@ -3,6 +3,6 @@ from collections import Counter
 SampleDictionary = {i: i ** 2 for i in range(1, 6)}
 print("Sample dictionary:", SampleDictionary)
 
-HighestValues = (Counter(SampleDictionary)).most_common(3) 
+HighestValues = (Counter(SampleDictionary)).most_common(3)
 
 print("Result dictionary with 3 highest values:", HighestValues)
