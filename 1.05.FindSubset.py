5c02d84901be1be866e0f93b99ced9c935be8d18
Aleksandr Berezin <32334657+denura@users.noreply.github.com>
2020-01-28 22:02:45 +0300

    1.05.FindSubset.py:21:0: C0325: Unnecessary parens after 'if' keyword (superfluous-parens)

--- 1.05.FindSubset.py
+++ 1.05.FindSubset.py
@@ -1,24 +1,25 @@
 RunTest = input("Run tests? (1/2/n): ")
-if RunTest == '1':
-    set1 = [1, 2]
-    set2 = [2, 3]
-    set3 = [2]
-elif RunTest == '2':
-    set1 = [1, 2]
-    set2 = [3, 4]
-    set3 = [5]
-elif RunTest == 'n':
-    set1 = input("Input the first set: ")
-    set2 = input("Input the second set: ")
-    set3 = input("Input the third set: ")
+if RunTest == "1":
+    Set1 = [1, 2]
+    Set2 = [2, 3]
+    Set3 = [2]
+elif RunTest == "2":
+    Set1 = [1, 2]
+    Set2 = [3, 4]
+    Set3 = [5]
+elif RunTest == "n":
+    Set1 = input("Input the first set: ")
+    Set2 = input("Input the second set: ")
+    Set3 = input("Input the third set: ")
 else:
     print("Wrong input!")
-    set1 = set2 = set3 = 0
+    Set1 = Set2 = Set3 = 0
 
-if set1:
-    print("Input sets:", set1, set2, set3)
+if Set1:
+    print("Input sets:", Set1, Set2, Set3)
     Result = "False"
-    if (set(set3).issubset(set1) & set(set3).issubset(set2)):
+    Subset = set(Set3).issubset(Set1) & set(Set3).issubset(Set2)
+    if Subset:
         Result = "True"
 
     print("The third set is a subset of the first and the second sets:", Result)
