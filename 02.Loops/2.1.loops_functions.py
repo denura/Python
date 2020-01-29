c722b439bf7ffe75bffdf6f860d11c0b2ee0f168
Aleksandr Berezin <32334657+denura@users.noreply.github.com>
2020-01-29 22:32:20 +0300

    2.1

--- 02.Loops/2.1.loops_functions.py
+++ 02.Loops/2.1.loops_functions.py
@@ -3,18 +3,19 @@
 """
 
 
-def is_even(number):
+def is_even(number: int):
     odd_counter = 0
-    for i in number:
-        if i % 2 == 0:
-            print("Четное число: ", i)
-        else:
+    i = 0
+    while i < number:
+        i += 1
+        if i % 2 > 0:
             print("Нечентное число: ", i)
             odd_counter += 1
             square = i * i
-            print("Квадрат нечетного числа %.d: = %.d" %(i, square))
-    print("Количество нечетных чисел: ", square)
+            print("Квадрат нечетного числа %.d = %.d" %(i, square))
+
+    print("Количество нечетных чисел: ", odd_counter)
 
 
-number = input("Введите верхнюю границу интервала: ")
+number = int(input("Введите верхнюю границу интервала: "))
 is_even(number)
