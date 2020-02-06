34162e37d032f57d2da93bd743300bf7e7a9a695
Aleksandr Berezin <32334657+denura@users.noreply.github.com>
2020-02-06 08:43:16 +0300

    4.5

---
+++ Lesson 4/website_alive/check_response.py
@@ -0,0 +1,19 @@
+"""5. Внутри модуля check_response напишите функцию, которая на вход принимает URL как строку
+(например, 'http://google.ru/') и использует функцию из предыдущего файла, чтобы сделать запрос.
+Если status_code равен 200, функция возвращает True, иначе False."""
+
+import make_request
+
+
+def get_url(url):
+    """функция на вход принимает URL как строку (например, 'http://google.ru/') и использует
+    функцию из предыдущего файла, чтобы сделать запрос. Если status_code равен 200, функция
+    возвращает True, иначе False."""
+    make_request.get(url)
+    print(make_request.status_code)
+    if make_request.status_code == 200:
+        print("True")
+        return True
+    else:
+        print("False")
+        return False
