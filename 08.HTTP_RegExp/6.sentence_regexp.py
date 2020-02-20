9716b259916ad03002abbedf3724a261c38175bb
denura <32334657+denura@users.noreply.github.com>
2020-02-20 22:54:52 +0300

    feat: add Python code for regexp

--- 08.HTTP_RegExp/6.sentence_regexp.py
+++ 08.HTTP_RegExp/6.sentence_regexp.py
@@ -5,3 +5,7 @@
 import re
 
 REG_EXP = re.compile('(\w{3,}\s?){4,}[?]')
+test_str = 'test test test test test? test test test test test?'
+iterator = re.finditer(REG_EXP, test_str)
+for match in iterator:
+    print('Совпадение найдено в позициях:', match.span())
